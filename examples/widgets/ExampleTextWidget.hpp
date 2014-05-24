7a60520050a63f3a62569c5ab3463ff562577d5d
falkTX <falktx@gmail.com>
2014-05-24 01:49:24 +0100

    Add very small text example

---
+++ examples/widgets/ExampleTextWidget.hpp
@@ -0,0 +1,75 @@
+/*
+ * DISTRHO Plugin Framework (DPF)
+ * Copyright (C) 2012-2014 Filipe Coelho <falktx@falktx.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with
+ * or without fee is hereby granted, provided that the above copyright notice and this
+ * permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+ * TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+ * NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef EXAMPLE_TEXT_WIDGET_HPP_INCLUDED
+#define EXAMPLE_TEXT_WIDGET_HPP_INCLUDED
+
+// ------------------------------------------------------
+// DGL Stuff
+
+#include "NanoWidget.hpp"
+
+// ------------------------------------------------------
+// use namespace
+
+using DGL::NanoWidget;
+using DGL::Window;
+
+// ------------------------------------------------------
+// our widget
+
+class ExampleTextWidget : public NanoWidget
+{
+public:
+    ExampleTextWidget(Window& parent)
+        : NanoWidget(parent),
+          fFont(createFont("sans", "./nanovg_res/Roboto-Regular.ttf"))
+    {
+        setSize(500, 300);
+    }
+
+protected:
+    void onDisplay() override
+    {
+        const int width  = getWidth();
+        const int height = getHeight();
+
+        beginFrame();
+        save();
+
+        fontSize(40.0f);
+        textAlign(Align(ALIGN_CENTER|ALIGN_MIDDLE));
+        textLineHeight(20.0f);
+
+        beginPath();
+        fillColor(RGBA(220,220,220,255));
+        roundedRect(10, height/4+10, width-20, height/2-20, 3);
+        fill();
+
+        fillColor(RGBA(0,0,0,220));
+        textBox(10, height/2, width-20, "Hello World!", nullptr);
+
+        restore();
+        endFrame();
+    }
+
+private:
+    FontId fFont;
+};
+
+// ------------------------------------------------------
+
+#endif // EXAMPLE_TEXT_WIDGET_HPP_INCLUDED
